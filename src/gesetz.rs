a8b615b4aabf18b3feb12be66b748511b9298fd0
Patrick Elsen <pelsen@xfbs.net>
2019-05-30 01:23:14 +0200

    updates

--- src/gesetz.rs
+++ src/gesetz.rs
@@ -2,25 +2,32 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_xml_rs;
 
-const API_TOC: &'static str = "https://www.gesetze-im-internet.de/gii-toc.xml";
-
 use serde::{Deserialize, Serialize};
 
+/// API endpoint to get current table of contents.
+const API_TOC: &'static str = "https://www.gesetze-im-internet.de/gii-toc.xml";
+
+/// Entry in the table of content of current laws.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Gesetz {
+pub struct TocItem {
+    /// The title of the law.
     pub title: String,
+    /// Link to the XML file with the contents of the law.
     pub link: String,
 }
 
+/// Table of content of current laws.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Toc {
+    /// List of items.
     #[serde(rename = "item", default)]
-    pub items: Vec<Gesetz>
+    pub items: Vec<TocItem>
 }
 
-impl Gesetz {
-    pub fn new(title: &str, link: &str) -> Gesetz {
-        Gesetz {
+impl TocItem {
+    /// Create new TocItem with given properties.
+    pub fn new(title: &str, link: &str) -> Self {
+        Self {
             title: title.into(),
             link: link.into(),
         }
@@ -28,28 +35,30 @@ impl Gesetz {
 }
 
 impl Toc {
+    /// Fetch the current table of contents from the server.
     pub fn fetch_toc() -> Result<String, Box<::std::error::Error>> {
         reqwest::get(API_TOC)?.text().map_err(|e| e.into())
     }
 
-    pub fn fetch() -> Result<Toc, Box<::std::error::Error>> {
+    /// Fetch the current table of contents from the server and parse it, yielding a Toc.
+    pub fn fetch() -> Result<Self, Box<::std::error::Error>> {
         Self::fetch_toc()
             .and_then(|s| serde_xml_rs::from_str(&s).map_err(|e| e.into()))
     }
 }
 
 #[test]
-fn test_can_create_gesetz() {
-    let gesetz = Gesetz::new("Abgeordnetengesetz", "ABG");
+fn test_can_create_toc_item() {
+    let gesetz = TocItem::new("Abgeordnetengesetz", "ABG");
     assert_eq!(gesetz.title, "Abgeordnetengesetz");
     assert_eq!(gesetz.link, "ABG");
 }
 
 #[test]
-fn test_can_compare_gesetz() {
-    let gesetz_a = Gesetz::new("A", "A");
-    let gesetz_b = Gesetz::new("B", "A");
-    let gesetz_c = Gesetz::new("A", "C");
+fn test_can_compare_toc_item() {
+    let gesetz_a = TocItem::new("A", "A");
+    let gesetz_b = TocItem::new("B", "A");
+    let gesetz_c = TocItem::new("A", "C");
     
     assert_ne!(gesetz_a, gesetz_b);
     assert_ne!(gesetz_a, gesetz_c);
