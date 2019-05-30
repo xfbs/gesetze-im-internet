32f693d7685162e1e15f70f5f21788ba266cfdfe
Patrick Elsen <pelsen@xfbs.net>
2019-05-30 15:35:59 +0200

    updates

--- src/bin/lawapi_de.rs
+++ src/bin/lawapi_de.rs
@@ -1,15 +1,23 @@
 extern crate clap;
 extern crate lawapi_de;
+extern crate regex;
 
-use clap::{App, AppSettings, ArgMatches, SubCommand};
-use lawapi_de::gesetz::{Toc, TocItem};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+use lawapi_de::gesetz::{Toc};
+use regex::Regex;
 
 fn main() {
     let matches = App::new("lawapi_de")
         .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(
             SubCommand::with_name("list")
-                .about("lists laws by fetching the table of contents"),
+                .about("lists laws by fetching the table of contents")
+                .arg(Arg::with_name("search")
+                     .short("s")
+                     .long("search")
+                     .help("searches for a string")
+                     .value_name("REGEX")
+                     .takes_value(true))
         )
         .subcommand(
             SubCommand::with_name("get")
@@ -25,12 +33,24 @@ fn main() {
 }
 
 fn list(matches: Option<&ArgMatches>) {
-    Toc::fetch()
-        .map(|toc|
-             for item in toc.items {
-                 println!("[{}] {}", item.short().unwrap_or("???"), item.title);
-             })
-        .map_err(|e| println!("{:?}", e));
+    let toc = Toc::fetch();
+
+    match toc {
+        Ok(toc) => {
+            let regex = if let Some(search) = matches.unwrap().value_of("search") {
+                Regex::new(search).ok()
+            } else {
+                None
+            };
+
+            for item in toc.items {
+                if regex.as_ref().map(|r| r.is_match(&item.title)).unwrap_or(true) {
+                    println!("[{}] {}", item.short().unwrap_or("???"), item.title);
+                }
+            }
+        },
+        Err(e) => println!("{:?}", e),
+    }
 }
 
 fn get(matches: Option<&ArgMatches>) {
