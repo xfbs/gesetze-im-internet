39db51919943351672f6128bd63e8053e728f9bb
Patrick Elsen <pelsen@xfbs.net>
2019-06-05 20:54:56 +0200

    adds test

--- src/tests.rs
+++ src/tests.rs
@@ -4,9 +4,10 @@ const TOC: &'static str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/dat
 
 #[test]
 fn test_load_toc() {
-    for _ in 0..10 {
-        let toc = Toc::from_str(TOC).expect("can't parse toc");
+    let toc = Toc::from_str(TOC).expect("can't parse toc");
 
-        assert_eq!(toc.items.len(), 6419);
-    }
+    assert_eq!(toc.items.len(), 6419);
+
+    assert_eq!(toc.items[0].title, "Gesetz über die Ausprägung einer 1-DM-Goldmünze und die Errichtung der Stiftung \"Geld und Währung\"");
+    assert_eq!(toc.items[0].link, "http://www.gesetze-im-internet.de/1-dm-goldm_nzg/xml.zip");
 }
